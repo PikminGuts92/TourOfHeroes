a1f224324c16a2b45a223c2a92980f0a23c04ce9
PikminGuts92 <PikminGuts92@gmail.com>
2017-05-03 16:48:02 -0400

    Moved hero and hero details to seperate files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,17 @@
 import { Component } from '@angular/core';
+import { Hero } from "./hero"
 
 @Component({
   selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <!-- Check if selected hero is null -->
-  <div *ngIf='selectedHero'>
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
-  <h2>My Heroes</h2>
-  <ul class='heroes'>
-    <li *ngFor='let hero of heroes' (click)='onSelect(hero)' [class.selected]='hero === selectedHero'>
-      <span class='badge'>{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class='heroes'>
+      <li *ngFor='let hero of heroes' (click)='onSelect(hero)' [class.selected]='hero === selectedHero'>
+        <span class='badge'>{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <hero-detail [hero]='selectedHero'></hero-detail>
   `,
   styles: [`
     .selected {
@@ -79,12 +72,6 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
-  //public toString(): string { return this.name };
 }
 
 const HEROES: Hero[] = [
---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+export class Hero {
+  id: number;
+  name: string;
+  //public toString(): string { return this.name };
+}
